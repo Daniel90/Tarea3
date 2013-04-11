26f774d228dfd136080d0d58988a6d30173889ad
johnlopez <jpx_soad@hotmail.com>
2013-04-10 23:15:11 -0300

    Update CyPostgreeSQL.cpp

--- CyPostgreeSQL.cpp
+++ CyPostgreeSQL.cpp
@@ -15,69 +15,59 @@ char *passwd = "root";
 int main(int argc, char *argv[])
 {
      int i;
+     float Promedio=0;
+     float DesvEst=0;
+     float Suma=0;
+     float value=0;
+     float A[100][100];
 
      cnn = PQsetdbLogin(host,port,NULL,NULL,dataBase,user,passwd);
 
     if (PQstatus(cnn) != CONNECTION_BAD) {
         cout << "Coneccion exitosa!" << endl;
-        cout << "host: "<<host<<endl<<"Puerto: "<<port<<endl<<"Base de datos: "<<dataBase<<endl<<"Usuario: "<<user<<endl<<"password: ****"<<endl;
         //result = PQexec(cnn, "SELECT AVG(nota) from asignaturas_cursadas");
-        result1 = PQexec(cnn, "Select docente_id as Codigo_Docente, avg(nota) as Promedio from cursos,asignaturas_cursadas where cursos.curso_id = asignaturas_cursadas.curso_id group by docente_id");
-        result2 = PQexec(cnn,"Select docente_id as Codigo_Docente, sqrt((sum(power((nota-(select avg(nota) from asignaturas_cursadas)),2)))/(select count(nota)-1 from asignaturas_cursadas)) as Desv_Estandar from cursos,asignaturas_cursadas where cursos.curso_id = asignaturas_cursadas.curso_id group by docente_id");
-        if (result1 != NULL) 
-        {
-            int tuplas = PQntuples(result1);
-            int campos = PQnfields(result1);
-           // cout << "No. Filas:" << tuplas << endl;
-           // cout << "No. Campos:" << campos << endl;
-
-            cout << endl <<"Promedio de notas por profesores:" << endl<<endl;
-
-            for (i=0; i<campos; i++) {
-                cout <<PQfname(result1,i) << "                   ";
-            }
-            cout<<endl;  
-            for (i=0; i<tuplas; i++) 
-            {
-                for (int j=0; j<campos; j++) 
-                {
-                    
-                    cout <<"      "<<PQgetvalue(result1,i,j)<<"            |";
-                    
-                }
-                cout << endl;
-            }
-            
-        }
+        result = PQexec(cnn, "SELECT c.curso_id,c.docente_id,a.nota FROM cursos as c,asignaturas_cursadas as a where c.curso_id = a.curso_id");
         
-        if (result2 != NULL) 
-        {
-            int tuplas = PQntuples(result2);
-            int campos = PQnfields(result2);
-           // cout << "No. Filas:" << tuplas << endl;
-           // cout << "No. Campos:" << campos << endl;
+        if (result != NULL) {
+            int tuplas = PQntuples(result);
+            int campos = PQnfields(result);
+            cout << "No. Filas:" << tuplas << endl;
+            cout << "No. Campos:" << campos << endl;
 
-            cout << endl <<"La desviacion estandar por profesores fue:" << endl<<endl;
+            cout << "Los nombres de los campos son:" << endl;
 
             for (i=0; i<campos; i++) {
-                cout <<PQfname(result2,i) << "                  ";
+                cout << PQfname(result,i) << " | ";
             }
-            cout<<endl;  
-            for (i=0; i<tuplas; i++) 
-            {
-                for (int j=0; j<campos; j++) 
-                {
+
+            cout << endl << "Contenido de la tabla" << endl;
+            //system("PAUSE");
+            for (i=0; i<tuplas; i++) {
+                for (int j=0; j<campos; j++) {
                     
-                    cout <<"      "<<PQgetvalue(result2,i,j) << "          |  ";
+                    //value = atof(PQgetvalue(result,i,j));
+                    //Suma=Suma+value;
+                    //cout << Suma << " | ";
+                    cout << PQgetvalue(result,i,j) << " | "<<i<< " | "<<j<< " | ";
+                    system("PAUSE");
+                    system("CLS");
+                    
+                    
+                    
+                    
+                    
+                    
+                    
                     
                 }
                 cout << endl;
             }
+            //cout << endl << "PROMEDIO :" <<Suma/tuplas<< endl;
             
         }
 
         // Ahora nos toca liberar la memoria
-        PQclear(result1);
+        PQclear(result);
         PQclear(result2);
 
     } else {
