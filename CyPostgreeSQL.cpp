45df5df5c19dd3ad0d384e8135b8a18888a8de43
Daniel Abrilot <daniel.abrilot@gmail.com>
2013-04-10 20:25:27 -0700

    Update CyPostgreeSQL.cpp

--- CyPostgreeSQL.cpp
+++ CyPostgreeSQL.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 PGconn *cnn = NULL;
-PGresult *result1 = NULL, *result2 = NULL;
+PGresult *result = NULL;
 
 char *host = "localhost";
 char *port = "5432";
@@ -19,36 +19,33 @@ int main(int argc, char * argv[])
     int i;
     cnn = PQsetdbLogin(host,port,NULL,NULL,dataBase,user,passwd);
     if (PQstatus(cnn) != CONNECTION_BAD) {
-        cout << "Estamos conectados a PostgreSQL!" << endl;
+        cout << "Conexion exitosa!" << endl;
+        cout << "host: "<<host<<endl<<"Puerto: "<<port<<endl<<"Base de datos: "<<dataBase<<endl<<"Usuario: "<<user<<endl<<"password: ****"<<endl;
         result = PQexec(cnn, "select c.curso_id, avg(a.nota) as Promedio,c.docente_id,sqrt((sum(power((nota-(select avg(a.nota) from asignaturas_cursadas as a)),2)))/(select count(a.nota)-1 from asignaturas_cursadas as a)) as Desv_Estandar from asignaturas_cursadas as a,cursos as c where c.curso_id = a.curso_id group by c.curso_id");
         if (result != NULL) {
             int tuplas = PQntuples(result);
             int campos = PQnfields(result);
-            cout << "No. Filas:" << tuplas << endl;
-            cout << "No. Campos:" << campos << endl<<endl;
-            cout << "Los nombres de los campos son:" << endl;
-            for (i=0; i<campos; i++) {
-                cout << PQfname(result,i) << " | ";
-            }
-            cout << endl<<endl;
             
-            cout<<"Contenido de la tabla" << endl;
+            cout << La informacion se muestra a continuacion:" << endl << endl;
+            
+            
+            
             for (i=0; i<campos; i++) {
-                cout << PQfname(result,i) << "       |       ";
+                cout << PQfname(result,i) << "          ";
             }
             cout<<endl;
             for (i=0; i<tuplas; i++) {
                 for (int j=0; j<campos; j++) {
-                    cout << PQgetvalue(result,i,j) << "       |       ";
+                    cout << PQgetvalue(result,i,j) << "         ";
                 }
                 cout << endl;
             }
         }
-        // Ahora nos toca liberar la memoria
+        
         PQclear(result);
     } else {
         cout << "Error de conexion" << endl;
-        return 0;
+       
     }
     PQfinish(cnn);
   system("PAUSE");
