7576cbc66c8ee742fadb83d53591087a82b6006c
johnlopez <jpx_soad@hotmail.com>
2013-04-10 23:18:22 -0300

    Promedio listo

--- CyPostgreeSQL.cpp
+++ CyPostgreeSQL.cpp
@@ -12,113 +12,43 @@ char *dataBase = "tarea";
 char *user = "postgres";
 char *passwd = "root";
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
-     int i;
-     float Promedio=0;
-     float DesvEst=0;
-     float Suma=0;
-     float value=0;
-     float A[1000][50];
-     
-     int cont=0;
-     float sum=0;
-     float prof=0;
-     float curs=0;
-     int chip=1;
-
-     cnn = PQsetdbLogin(host,port,NULL,NULL,dataBase,user,passwd);
-
+    int i;
+    cnn = PQsetdbLogin(host,port,NULL,NULL,dataBase,user,passwd);
     if (PQstatus(cnn) != CONNECTION_BAD) {
-        cout << "Coneccion exitosa!" << endl;
-        //result = PQexec(cnn, "SELECT AVG(nota) from asignaturas_cursadas");
-        result = PQexec(cnn, "SELECT c.curso_id,c.docente_id,a.nota FROM cursos as c,asignaturas_cursadas as a where c.curso_id = a.curso_id");
-        
+        cout << "Estamos conectados a PostgreSQL!" << endl;
+        result = PQexec(cnn, "select c.curso_id, avg(a.nota) as Promedio,c.docente_id from asignaturas_cursadas as a,cursos as c where c.curso_id = a.curso_id group by c.curso_id");
         if (result != NULL) {
             int tuplas = PQntuples(result);
             int campos = PQnfields(result);
             cout << "No. Filas:" << tuplas << endl;
-            cout << "No. Campos:" << campos << endl;
-
+            cout << "No. Campos:" << campos << endl<<endl;
             cout << "Los nombres de los campos son:" << endl;
-
             for (i=0; i<campos; i++) {
                 cout << PQfname(result,i) << " | ";
             }
-
-            cout << endl << "Contenido de la tabla" << endl;
-            //system("PAUSE");
+            cout << endl<<endl;
+            
+            cout<<"Contenido de la tabla" << endl;
+            for (i=0; i<campos; i++) {
+                cout << PQfname(result,i) << "       |       ";
+            }
+            cout<<endl;
             for (i=0; i<tuplas; i++) {
                 for (int j=0; j<campos; j++) {
-                    
-                    value = atof(PQgetvalue(result,i,j));
-                    A[i][j]=value;
-                    //cout << PQgetvalue(result,i,j) << " | "<<i<< " | "<<j<< " | ";
-                    //system("PAUSE");
-                    //system("CLS");
-                                
+                    cout << PQgetvalue(result,i,j) << "       |       ";
                 }
                 cout << endl;
             }
-            for (i=0; i<tuplas; i++) {
-                for (int j=0; j<campos; j++) {
-                    
-                    cout << A[i][j] << " | ";
-                                                   
-                }
-                cout << endl;
-            }
-            
-            system("PAUSE");
-            
-            for (i=0; i<tuplas; i++) {
-                for (int j=0; j<campos; j++) {
-                    
-                    if(j=0)
-                    {      if(A[i][0]==A[i-1][0])
-                           {        cont++;
-                                    sum=sum+A[i][2];
-                                    prof=A[i][1];
-                                    curs=A[i][0];
-                           }
-                           else
-                           {        cout<<"Curso: "<<curs<<" Profesor: "<<prof<<" Promedio: "<<sum/cont<<endl;
-                                    system("PAUSE");
-                                    chip=1;
-                                    cont=0;
-                                    sum=0;
-                           }
-                           if(chip==1)
-                           {        cont++;
-                                    sum=sum+A[i][2];
-                                    prof=A[i][1];
-                                    curs=A[i][0];
-                                    chip=0;
-                           }
-                           
-                    }
-                                                   
-                }
-                cout << endl;
-            }
-            
-            //cout << endl << "PROMEDIO :" <<Suma/tuplas<< endl;
-            
         }
-
         // Ahora nos toca liberar la memoria
         PQclear(result);
-        PQclear(result2);
-
     } else {
-        cout << "Error de conexion" << endl<<PQerrorMessage(cnn);
-        
-        
+        cout << "Error de conexion" << endl;
+        return 0;
     }
-
     PQfinish(cnn);
-
-  
-    system("PAUSE");
+  system("PAUSE");
     return EXIT_SUCCESS;
 }
